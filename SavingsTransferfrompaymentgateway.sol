e3b15fcb39abc81a492e1f9254d6078c9177e503
Gomathi T <62291940+Gomathi1806@users.noreply.github.com>
2023-07-20 12:55:39 +0100

    Update SavingsTransferfrompaymentgateway.sol

--- SavingsTransferfrompaymentgateway.sol
+++ SavingsTransferfrompaymentgateway.sol
@@ -42,3 +42,34 @@ contract PaymentGateway {
 
 // Note: provides a payment gateway where the funds are split into savings and the rest. Whenever a payment is made to this contract, the amount is automatically distributed according to the predefined percentage.
 // we can implement this contract by setting up external oracle server like chain link keepers and off-chain oracle server should have ether to pay the transaction fee.
+
+Simple oracle contract or just an representation contract:
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+contract SavingsContract {
+    address payable public owner;
+    address payable public savingsAccount;
+    
+    constructor (address payable _owner, address payable _savingsAccount) {
+       
+ owner = _owner;
+         savingsAccount = _savingsAccount;
+    }
+
+    function transferFunds(uint _amount) external payable {
+        uint256 amountToSave = _amount;
+        require(msg.value >= amountToSave, "Not enough funds");
+
+        // transfer the required amount to the savings account
+        savingsAccount.transfer(amountToSave);
+
+        // return remaining funds back to the income account
+        uint256 remainingFunds = msg.value - amountToSave;
+        if(remainingFunds > 0){
+            msg.sender.transfer(remainingFunds);
+        }
+     }
+}
+
